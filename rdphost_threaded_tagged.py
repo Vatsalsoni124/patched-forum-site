f75f5f0f2b99550f50d41b75eff86a6421e2ed0c
Vatsalsoni124 <138987424+Vatsalsoni124@users.noreply.github.com>
2025-05-23 00:58:46 +0530

    rdphost_threaded_tagged.py

--- rdphost_threaded_tagged.py
+++ rdphost_threaded_tagged.py
@@ -1,2 +1,4 @@
+import sys
+combo_file = sys.argv[1] if len(sys.argv) > 1 else "combo.txt"
 # Placeholder for rdphost_threaded_tagged.py
 # Please paste your previously downloaded script here again for use in deployment
